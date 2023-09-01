6d97859302c12787270c2b49e33ebf138124042b
Dmitrii Zolotuhin <dmitrii.zolotuhin@agmatix.com>
2023-09-01 11:51:45 +0300

    Fixed test

--- Assets/Tests/HealthSystemTest.cs
+++ Assets/Tests/HealthSystemTest.cs
@@ -13,13 +13,7 @@ public class HealthSystemTest
         Assert.AreEqual(1, 1);
     }
     
-    [Test]
-    public void HealthSystemTestSimpleFails()
-    {
-      Assert.AreEqual(1, 2);
-    }
-
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+   // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
     [UnityTest]
     public IEnumerator HealthSystemTestWithEnumeratorPasses()

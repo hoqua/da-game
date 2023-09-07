abb3236a022d6082661b8a488bec2d0f623290af
Dmitrii Zolotuhin <dmitrii.zolotuhin@agmatix.com>
2023-09-07 09:39:33 +0300

    Added test assemblies

--- Assets/Scr/Unit/Health.cs
+++ Assets/Scr/Unit/Health.cs
@@ -1,40 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour
-{
-    public int _maxHealth = 100;
-    private int _healthPoints;
-    private bool _isDead;
-    
-    private Health(int startingHealth)
-    {
-        _healthPoints = startingHealth;
-    }
+public class Health : MonoBehaviour {
+  public int _maxHealth = 100;
+  private int _healthPoints;
+  private bool _isDead;
 
-    private void TakeDamage(int damageAmount)
-    {
-        _healthPoints -= damageAmount;
-        
-        if (_healthPoints <= 0)
-            IsDead();
-    }
+  public Health(int startingHealth) {
+    _healthPoints = startingHealth;
+  }
 
-    private void Heal(int healAmount)
-    {
-        if (_isDead == false)
-        {
-            _healthPoints += healAmount;
-        }
-        if (_healthPoints > _maxHealth)
-        {
-            _healthPoints = _maxHealth;
-        }
-    }
-    private void IsDead()
-    {
-        _healthPoints = 0;
-        _isDead = true;
-    }
+  private void TakeDamage(int damageAmount) {
+    _healthPoints -= damageAmount;
+
+    if (_healthPoints <= 0)
+      IsDead();
+  }
+
+  private void Heal(int healAmount) {
+    if (_isDead == false) _healthPoints += healAmount;
+    if (_healthPoints > _maxHealth) _healthPoints = _maxHealth;
+  }
+
+  private void IsDead() {
+    _healthPoints = 0;
+    _isDead = true;
+  }
 }
--- Assets/Tests/HealthSystemTest.cs
+++ Assets/Tests/HealthSystemTest.cs
@@ -1,25 +1,21 @@
 using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
-using UnityEngine;
 using UnityEngine.TestTools;
 
-public class HealthSystemTest
-{
-    // A Test behaves as an ordinary method
-    [Test]
-    public void HealthSystemTestSimplePasses()
-    {
-        Assert.AreEqual(1, 1);
-    }
-    
-   // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator HealthSystemTestWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
-    }
+public class HealthSystemTest {
+  // A Test behaves as an ordinary method
+  [Test]
+  public void HealthSystemTestSimplePasses() {
+    var health = new Health(100);
+    Assert.AreEqual(health._maxHealth, 100);
+  }
+
+  // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+  // `yield return null;` to skip a frame.
+  [UnityTest]
+  public IEnumerator HealthSystemTestWithEnumeratorPasses() {
+    // Use the Assert class to test conditions.
+    // Use yield to skip a frame.
+    yield return null;
+  }
 }
